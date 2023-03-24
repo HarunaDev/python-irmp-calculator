080cd2ebdca4a52e8b57a4c1e83efede5d9c890c
Alvin Haruna <harunaalvin69@gmail.com>
2023-03-24 18:13:54 +0100

    added code to print app details and get data from user

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 # collect input needed for app, input: principal, apr(annual interest rate), years
+def main():
+    print("This is a monthly payment calculator\n\nPlease enter the required fields to calculate your monthly interest payment.")
+
+    principal = float(input("Enter principal amount: "))
+    apr = float(input("Enter annual interest rate: "))
+    years = int(input("Enter years: "))
 
 # calculate monthly payment
 
